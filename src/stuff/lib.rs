634d0a721d548bb0b3f409de0fe1eaf76afd3437
Hans Jørgen Hoel <hansjorg@gmail.com>
2014-06-27 19:15:24 +0200

    Fix

--- src/stuff/lib.rs
+++ src/stuff/lib.rs
@@ -1,18 +1,16 @@
 // the "link" crate attribute is currently required for rustdoc, but normally
 // isn't needed.
-#[crate_id = "universe"];
-#[crate_type="lib"];
+#![crate_id = "universe"]
+#![crate_type="lib"]
 
-//! Tools for dealing with universes (this is a doc comment, and is shown on
-//! the crate index page. The ! makes it apply to the parent of the comment,
-//! rather than what follows).
+use std::string::String;
 
 /// Widgets are very common (this is a doc comment, and will show up on
 /// Widget's documentation).
 pub struct Widget {
     /// All widgets have a purpose (this is a doc comment, and will show up
     /// the field's documentation).
-    purpose: ~str,
+    purpose: String,
     /// Humans are not allowed to understand some widgets
     understandable: bool
 }
